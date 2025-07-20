6ca74ed04a369294acd996ce4fb3c2b9c2df783d
Sakthi Bharathi <sakthibharathipcmr@gmail.com>
2025-07-20 19:30:11 +0530

    Revert "1"
    This reverts commit 6ed9aa52108def7e6d39bad7696637e1076dd431.

---
+++ Assets/TestMain.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Main Worked");
+        Debug.Log("New Feature Worked");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

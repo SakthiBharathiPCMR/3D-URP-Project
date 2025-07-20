7facc52d3c0398dbd4d6f992f370f6ee5a2a758a
Sakthi Bharathi <sakthibharathipcmr@gmail.com>
2025-07-20 19:11:50 +0530

    Update TestMain.cs

--- Assets/TestMain.cs
+++ Assets/TestMain.cs
@@ -7,7 +7,7 @@ public class TestMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Main Worked");
+        Debug.Log("Main Worked2");
     }
 
     // Update is called once per frame
